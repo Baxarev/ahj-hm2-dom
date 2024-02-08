cf708b48934a564bb572c15037ad05002cfb1600
«Илья» <baxarev.ilya@mail.ru>
2024-02-08 14:35:00 +0300

    ajs-hm2-end

--- src/js/app.js
+++ src/js/app.js
@@ -2,10 +2,16 @@ document.addEventListener("DOMContentLoaded", () => {
   const boxCollection = document.querySelectorAll(".box__item");
 
   function setRandomImg() {
+    const getRandom = () => Math.floor(Math.random() * boxCollection.length);
+
+    let count = getRandom();
+
+    while (boxCollection[count].classList.contains("box__item_active")) {
+      count = getRandom();
+    }
+
     boxCollection.forEach((i) => i.classList.remove("box__item_active"));
-    boxCollection[
-      Math.floor(Math.random() * boxCollection.length)
-    ].classList.add("box__item_active");
+    boxCollection[count].classList.add("box__item_active");
   }
 
   setInterval(setRandomImg, 1000);
